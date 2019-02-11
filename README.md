75cfcf05230bba134c484f76fbd915dc03cf33f1
rohan <rshanker779@gmail.com>
2019-02-11 22:06:42 +0000

    Updating build and docs for changes

--- README.md
+++ README.md
@@ -1,11 +1,17 @@
-# conways-game-of-life
-Simple python implementation of Conway, with a numpy engine and matplotlib graphics.
+# cellular-automate
+Python implementations of two simple cellular automata, with a numpy engine and matplotlib graphics.
+Includes Conway's game of life and a terrain generation script.
 
-To run, run
+To install
 ```
 python setup.py install
-python conway.py
 ``` 
+and the run either (or both)
+```
+python conway/conway.py
+python terrain-generation/terrain.py
+```
+
 
 Note dependencies are restrictive and project will likely work with lower versions of
 python, numpy and matplotlib.
--- setup.py
+++ setup.py
@@ -3,15 +3,15 @@ from setuptools import setup, find_namespace_packages
 with open("README.md", "r") as f:
     long_description = f.read()
 
-setup(name="conways-game-of-life",
+setup(name="cellular_automata",
       version='1.0.0',
       author="rshanker779",
       author_email="rshanker779@gmail.com",
-      description="Simple python implementation of Conway's game" \
-                  "of life",
+      description="Cellular automate in python, including implementation of Conway's game" \
+                  "of life and some terrain generation",
       long_description=long_description,
       long_description_content_type="text/markdown",
-      url="https://github.com/rshanker779/conways-game-of-life/tree/master",
+      url="https://github.com/rshanker779/cellular_automata/tree/master",
       license='MIT',
       python_requires='>=3.5',
       install_requires=['numpy',
